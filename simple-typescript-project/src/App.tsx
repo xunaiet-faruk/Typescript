58ffab25078adba3de3e82e35e2d1cf9f54dec6b
junaietfaruk <junaietfaruk@throughltd.com>
2026-05-26 11:58:03 +0600

    objectg and array type

--- simple-typescript-project/src/App.tsx
+++ simple-typescript-project/src/App.tsx
@@ -5,12 +5,18 @@ import './index.css'
 
 function App() {
 
+  const user1 ={
+  name: "Junaiet faruk",
+  age: 25,
+  isLogged: true,
+  lang: ["Bangla","English","Hindi"]
+  }
 
   return (
   <>
   
   <h1>Hello Typescript + Vite</h1>
-  <Users name="Junaiet faruk" age={25} isLogged={true} />
+  <Users user={user1}/>
   </>
   )
 }
--- simple-typescript-project/src/Componet/Users.tsx
+++ simple-typescript-project/src/Componet/Users.tsx
@@ -1,11 +1,23 @@
-type UserProps = { name: string, age: number, isLogged: boolean }
-const Users = ({ name, age, isLogged }: UserProps) => {
+type UserProps = {    
+    user: {
+        name: string;
+        age: number;
+        isLogged: boolean;
+        lang: string[];
+    }
+}
+const Users = ({ user }: UserProps) => {
     return (
         <div>
             <h2>Users</h2>
-            <p>The user name is {name}</p>
-            <p>The user age is {age}</p>
-            <p>The user is {isLogged ? 'logged in' : 'not logged in'}</p>
+            <p>The user name is {user.name}</p>
+            <p>The user age is {user.age}</p>
+            <p>The user is {user.isLogged ? 'logged in' : 'not logged in'}</p>
+            <div>
+               This user speaks {user.lang.map((language,index)=>{
+                    return <p key={index}>{language}</p>
+                })}
+            </div>
         </div>
     );
 };
