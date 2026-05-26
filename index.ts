ba256549da2723b25ddb820c3fabc0a70fff8ac4
junaietfaruk <junaietfaruk@throughltd.com>
2026-05-26 11:19:55 +0600

    simple project typescript and react

--- index.ts
+++
@@ -1,2 +0,0 @@
-let name ="junaiet faruk"
-console.log(name);
---
+++ simple-typescript-project/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './index.css'
+
+function App() {
+
+
+  return (
+  <>
+  
+  <h1>Hello Typescript + Vite</h1>
+  
+  </>
+  )
+}
+
+export default App
